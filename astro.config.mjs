b937733eb0df0aba8481bfb5e6aa02def752e09e
chhatreshkhatri <chhatresh.khatri1002@gmail.com>
2023-11-04 14:39:30 +0530

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.chhatreshkhatri.com',
+  site: "https://www.chhatreshkhatri.com",
   integrations: [tailwind(), sitemap()]
 });
