8fda96236d8e63f07fba2a5ac68cd4c25cbbda06
Chhatresh Khatri <chhatresh.khatri1002@gmail.com>
2025-01-01 11:56:44 +0530

    Preview (#3)
    * styling and preview
    
    * styling
    
    * index
    
    * package update
    
    * SSR with cloudfalre
    
    * adapter
    
    * log api
    
    * cloudflare images service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.chhatreshkhatri.com",
   integrations: [tailwind(), sitemap({ lastmod: new Date() })],
+  adapter: cloudflare({
+    imageService: "cloudflare",
+  }),
+  output: "server",
 });
