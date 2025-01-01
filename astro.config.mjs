94a1da747f82ec5e245d4ab87b4df9a8dced865f
Chhatresh Khatri <Chhatresh.Khatri1002@gmail.com>
2025-01-01 10:52:36 +0530

    SSR with cloudfalre

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.chhatreshkhatri.com",
   integrations: [tailwind(), sitemap({ lastmod: new Date() })],
+  output: "server",
+  adapter: cloudflare()
 });
