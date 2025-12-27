9549f996eb386ba58246fb665f244cc9eafc37f5
Chhatresh Khatri <chhatresh.khatri1002@gmail.com>
2025-12-27 16:49:13 +0530

    minor update

--- packageUpdate.cmd
+++ packageUpdate.cmd
@@ -1,1 +1,1 @@
-npm i @astrojs/cloudflare@latest @astrojs/mdx@latest @astrojs/rss@latest @astrojs/sitemap@latest @tailwindcss/vite astro@latest sharp@latest tailwindcss@latest @types/node@latest wrangler@latest
+npm i @astrojs/cloudflare@latest @astrojs/mdx@latest @astrojs/rss@latest @astrojs/sitemap@latest @tailwindcss/vite@latest astro@latest sharp@latest tailwindcss@latest @types/node@latest wrangler@latest
