52899db02f6a6aa3e457a99d9d3d90dbe12db974
Chhatresh Khatri <chhatresh.khatri1002@gmail.com>
2026-09-14 22:59:59 +0530

    Preview
    * upgrade

--- packageUpdate.cmd
+++
@@ -1,1 +0,0 @@
-npm i @astrojs/cloudflare@latest @astrojs/rss@latest @astrojs/sitemap@latest @tailwindcss/vite@latest astro@latest tailwindcss@latest @types/node@latest wrangler@latest
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,18 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+
+declare namespace App {
+  interface Locals extends Runtime {}
+}
+
 interface ImportMetaEnv {
-    readonly PUBLIC_API_URL: string;
-    readonly PUBLIC_visitsCounter: string;
-    // more env variables...
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
+  readonly PUBLIC_API_URL: string;
+  readonly PUBLIC_visitsCounter: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
